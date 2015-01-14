2f17934e3f2b635fbb76b92f179dd2769723e45e
Etay Luz <etayluz@gmail.com>
2015-01-13 22:08:38 -0500

    Save work

---
+++ WikiApp/WikiTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WikiTableCellTableViewCell.h
+//  WikiApp
+//
+//  Created by Etay Luz on 1/13/15.
+//  Copyright (c) 2015 com.desk.wikiapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WikiTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *pageTitle;
+@end
---
+++ WikiApp/WikiTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  WikiTableCellTableViewCell.m
+//  WikiApp
+//
+//  Created by Etay Luz on 1/13/15.
+//  Copyright (c) 2015 com.desk.wikiapp. All rights reserved.
+//
+
+#import "WikiTableViewCell.h"
+
+@implementation WikiTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
